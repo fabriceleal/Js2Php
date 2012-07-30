2978247c2a9756dc930410d5adb9939f5f16bd20
Fabrice Leal vmUbuntu <fabrice.leal.ch@gmail.com>
2012-07-30 13:39:41 -0700

    a lot of changes to the peg, testing ...

--- parse.js
+++ parse.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-with(require('./js.js').parser) {
+with(require('./newjs.js').parser) {
 
 	require('fs').readFile(process.argv[2], "utf-8", function(err, data){
 		if(err) throw err;
