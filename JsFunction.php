aff588e70a94eee1e576ed92d26dd9946c8ed870
Fabrice Leal vmUbuntu <fabrice.leal.ch@gmail.com>
2012-08-08 14:13:26 -0700

    added invoke special method, now u can call it as a function!

--- JsFunction.php
+++ JsFunction.php
@@ -8,8 +8,20 @@ class JsFunction {
 		$this->arguments = null;
 		$this->defaultThis = $this;
 	}
+
+	function __invoke(){
+		// Get args for the invocation
+		$args = func_get_args();
+
+		// Insert null as first parameter
+		array_unshift($args, null);
+
+		// Call the "call" method
+		return call_user_func_array(array($this, 'call'), $args);
+	}
 	
-	function call($newThis, $args_as_var_args = null){
+	function call($newThis){
+		print_r(func_get_args());
 		$head = array( is_null($newThis) ? $this->defaultThis : $newThis );
 		//print_r($head);
 		return call_user_func_array($this->fun, ($head + array_slice(func_get_args(), 0)) );
@@ -40,12 +52,12 @@ class JsFunction {
 
 }
 
-/*
+
 $x = new JsFunction(function($self, $a, $b){
 		print "\$self = $self, \$a = $a, \$b = $b\n"; 
 		return $a + $b; 
 	} );
-
+/*
 print "test 1\n";
 print "Should print \"\$self = [JsFunction], \$a = 1, \$b = 2\", result = 3:\n";
 print $x->call(null, 1, 2) . "\n";
@@ -59,11 +71,12 @@ $y = new JsFunction(function($self, $a){
 			return $a + $b;
 		}
 	);
-});
-
+});*/
+/*
 print "closure test\n";
 print "Should print 3:\n";
 print ($y->call(null, 1)->call(null, 2)) . "\n";
 */
+print "Sum = " . $x(1, 2) . "\n";
 
 ?>
