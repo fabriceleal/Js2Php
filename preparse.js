1774c2a1e0fafbed9bdad315b2d0e36f8893ed45
Fabrice Leal vmUbuntu <fabrice.leal.ch@gmail.com>
2012-09-05 15:26:58 -0700

    now im on track

--- preparse.js
+++ preparse.js
@@ -6,168 +6,168 @@
 
 (function(){
 
-	var recursiveFind = function(tree, tag, scopeAware){
-		var ret = [];
-		var matchTag;
-		if(tag instanceof Array){
-			matchTag = function(toMatch){ return tag.indexOf(toMatch) > -1; };
-		} else {
-			matchTag = function(toMatch){ return tag === toMatch; };
-		}
+	// Use this to add more output / suppress output
+	(function(){
+		var __log = console.log;
+		var __warn = console.warn;
 
-		if(scopeAware === undefined) scopeAware = false;
+		console.log = function(){
+			var args = Array.prototype.slice.call(arguments, 0);
+			args.unshift("[*] PREPARSE LOG:");
 
-		if((tree instanceof Array) || (tree instanceof Object)){
-			//console.log('is_array');			
-			for(var k in tree){
-
-				if(scopeAware && tree[k].tag === 'function_literal')
-					continue;
-
-				ret = ret.concat(recursiveFind(tree[k], tag));
-			};			
-		}
-
-		if((tree instanceof Object) && (tree.hasOwnProperty('tag')) && matchTag(tree.tag)){
-			ret.push(tree);
-		}
-
-		return ret;
-	};
-	
-
-	var instr_parser = {
-		"ref" : function(locals, captured, v) {
-			if(locals.indexOf(v.name) === -1 && captured.indexOf(v.name) === -1)
-			{
-				captured.push(v.name);
-			}
-			// no return
-		},
-
-		"decl" : function(locals, captured, v) {
-			locals.push(par.name);
-			// no return
-		},
-
-		"varStatement" : function(locals, captured, v) {
-			return v.value.map(function(decl){
-				var varName = decl.name.name;
-
-				if(locals.indexOf(varName) === -1)
-					locals.push(varName);
-
-				return v.init;
-			});
-		},
-		"functionDecl" : function(locals, captured, v) {
-			// if the function has a name, put in locals
-			if(v.name && locals.indexOf(v.name.name) === -1)
-				locals.push(v.name.name);
-			
-			
-		}
-	};
-
-	var preParseBlock = function(tree) {
-		// function body
-	};
-
-	var preParse = function(tree) {
-		// Top level parse
-
-		var locals = [];
-		
-		var parseInst = function(inst){
-			if(inst instanceof Array){
-				inst.forEach(parseInst);
-			} else if (inst) {
-				var f = instr_parser[inst.tag];
-				if(f) {
-					parseInst(f(locals, [], inst))
-				} else {
-					console.warn('Tag ' + inst.tag + ' is not implemented');
-				}
-			}
+			__log.apply(console, args);
 		};
-		parseInst(tree);		
+		console.warn = function(){
+			var args = Array.prototype.slice.call(arguments, 0);
+			args.unshift("[*] PREPARSE WARN:");
+			__warn.apply(console, args);
+		};
+	})();
+	
+	var initEnv = { locals:{ }, inner:undefined };
+
+	// Generic heavy-weight parser
+	var walk = function(tree, env, captured) {
+		for(var k in tree) {
+			if(tree[k] instanceof Object && tree[k].hasOwnProperty('tag')) {
+				parser(tree[k], env, captured);
+			}
+			if(tree[k] instanceof Array) {
+				tree[k].forEach(function(i){
+					parser(i, env, captured);
+				});
+			}
+		}
+
 	};
 
-/*
-	var preParse = function(tree){
-		recursiveFind(tree, ['functionDecl', 'functionExpr']).forEach(function(item){
+	// The instruction's parsers
+	var inst_parser = {
+		'decl': function(){
+			throw new Error('declared vars should be better handled!');
+		},
+		'ref' : function(tree, env, captured) {
+			console.log('Reference to var ' + tree.name + ', env: ' + JSON.stringify(env, null, 3));
 
-			// Assume *all* ids not declared inside as captured
-			var locals = [ 'this' ], captured = [];
+			// If var is not present in local scope, assume its captured
+			if(! env.locals.hasOwnProperty(tree.name)) {
+				//captured.locals[tree.name] = true;
+			}
+		},
+		'add;expression;return;assignment' : walk,
+		'functionDecl;functionExpr' : function(tree, env, captured) {  
 
-			// Add current function's parameters to locals
-			item.pars.value.forEach(function(par){
-				locals.push(par.name);
+			// Create a new env, which will contain the function's vars
+			var newEnv = { locals:{ }, inner:env };
+
+			// If the function has a name, put it in the current env
+			if(tree.name) {
+				if(tree.tag === 'functionExpr') {
+					if(! (tree.name.name instanceof String))
+						throw new Error('Expression function name is not a string!' + JSON.stringify(tree.name.name))
+
+					// If this is a functionExpr, the name of the function 
+					// will be visible only in its own scope
+					newEnv.locals[tree.name.name] = true;
+				} else if(tree.tag === 'functionDecl') {
+					if(typeof tree.name.name !== "string")
+						throw new Error('Declared function name is not a string!' + JSON.stringify(tree.name.name))
+
+					// Functions declared with a name are visible in the global scope
+					env.locals[tree.name.name] = true;
+				}
+			}
+			
+			if(tree.pars) {
+				tree.pars.value.forEach(function(v) {
+					if(typeof v.name !== "string")
+						throw new Error('Parameter name is not a string!' + JSON.stringify(v.name))
+
+					newEnv.locals[v.name] = true;
+				});
+			}
+			
+			// New captured context (...)
+			var newCaptured = { locals:{ }, inner:captured };
+			
+			// Parse functions body
+			tree.body.code.forEach(function(i) { 
+				parser(i, newEnv, newCaptured); 
 			});
-
-			console.log('* pars = ' + JSON.stringify(locals));
-
-			var instr_parser = {
-				"ref" : function(v) {
-					if(locals.indexOf(v.name) === -1 && captured.indexOf(v.name) === -1)
-					{
-						captured.push(v.name);
-					}
-				},
-				"decl" : function(v) {
-					locals.push(par.name);
-				}				
-			};
-
-			// Parse each function instruction
-			item.body.code.forEach(function parseElem(instruction){
-				// No instruction = no processing
-				if(instruction == null && instruction == ""){
-					return;
+			
+			// * Here we have all the information about the vars captured *
+			
+		},
+		'varStatement' : function(tree, env, captured) {
+			var newVars = [];
+			
+			tree.value.forEach(function(decl) {
+				// Search for references in the init expressions
+				if(decl.init) {
+					parser(decl.init.value, env, captured);
 				}
+				
+				newVars.push(decl.name.name);
+			});
+			
+			// Now update the current env with the declared vars
+			newVars.forEach(function(v) { 
+				if(typeof v !== "string")
+					throw new Error('Parameter name is not a string!' + JSON.stringify(v))
 
-				console.log('** instr: ' + JSON.stringify(instruction));
+				env.locals[v] = true; 
+			});			
+		}
+	};
 
-				var p = instr_parser[instruction.tag];
-				if(p){
-					var tmp = p(instruction);
-					if(tmp instanceof Array){
-						tmp.forEach(parseElem);
-					} else {
-						parseElem( tmp );
-					}
-				}
-*/
-/*
-				// 1st look for refs. If they are not declared in locals, push to all_refs
-				// TODO Multiple declaration var a = b = c = 1 will break here. FIX the grammar.
-				// FIXME function(a){ return function(b){ return a + b}} breaks here.
+	// To avoid code replication, I used a special notation in the obj's keys,
+	// that is unfolded here. No real processing here.
+	for(var k in inst_parser) {
+		(function(){
+			var agg = k;
+			var splitted = agg.split(";");
 
-				recursiveFind(instruction, 'id', true).forEach(function(ref){
-					if(locals.indexOf(ref.name) === -1 && captured.indexOf(ref.name) === -1){
-						captured.push(ref.name);
-					}
+			if(splitted.length > 1) {
+				var obj = inst_parser[agg];
 
+				splitted.forEach(function(key){
+					//console.log('Defining the getter of "' + key + '" as the same of "' + agg + '"');
+					inst_parser.__defineGetter__(key, function(){ return obj; });
 				});
 
-				if(instruction.tag === "let_collection"){
+				// Now this is garbage ...
+				inst_parser[agg] = undefined;
+			}
+		})();		
+	}
 
-					recursiveFind(instruction, 'let').forEach(function(decl){
-						locals.push(decl.id.name);
-					});
+	// The AST parser. Looking for captured vars...
+	var parser = function(tree, env, captured){
+		if(! tree)
+		{
+			console.warn('will skip null elem...'); return;
+		}
 
-				}
-*/
+		if(tree instanceof Array)
+		{
+			tree.forEach(function(elem) { return parser(elem, env, captured); }); return;
+		}
 
-//			});
-			//console.log(locals);
-			//console.log(captured);
+		if(! tree.tag)
+		{
+			console.warn('will skip elem without tag...'); return;
+		}
 
-			// Store in function structure
-/*			item.captured = captured;			
-		});
+		var f = inst_parser[tree.tag];
+		if(! f) {
+			console.warn('will skip elem ("' + tree.tag + '") without callback...'); return;
+		}
+
+		f(tree, env, captured);
 	};
-*/
-	exports.preParse = preParse;
+	
+	exports.preParse = (function(){ return function(tree){ 
+		parser(tree, initEnv, undefined);
+	}})();
 
 })();
